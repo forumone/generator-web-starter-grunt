8773584321a3c2bbec028cecddb1cd28bf1ee44d
Thibaut Lassalle <thibaut.lassalle@gmail.com>
2016-04-15 16:59:05 -0400

    add grunt task for generator-web-starter-grunt

--- web-starter/index.js
+++ web-starter/index.js
@@ -7,37 +7,37 @@ var generators = require('yeoman-generator'),
   glob = Promise.promisify(require('glob')),
   GruntfileEditor = require('gruntfile-editor');
 
+var gruntTasks = [];
+
 module.exports = generators.Base.extend({
   initializing : function() {
-    // TODO the following line goes in generator-web-starter initializing context
-    // this.options.parent.gruntDependencies = {};
-    this.options.gruntDependencies = {};
-    
-    // TODO the following line goes in generator-web-starter-wordpress initializing context
-    // this.options.parent.gruntDependencies['web-starter-grunt'] = {'compass' : {}, 'copy' : {}};
-    this.options.gruntDependencies['web-starter-grunt'] = {'compass' : {}, 'copy' : {}};
+    var that = this;
+    this.options.addPlugin("grunt", {
+      addGruntTasks : function(task, pluginContext) {
+        gruntTasks.push({'task' : task, 'pluginContext' : pluginContext});
+      },
+      addGruntDevDependency : function(dep) {
+        var pf = that.options.parent.answers.package_file;
+        if(pf.devDependencies.constructor !== Array) {
+          pf.devDependencies = [pf.devDependencies];
+        }
+        pf.devDependencies.push(dep);
+      }
+    });
   },
-
   writing : {
     settings : function() {
       var done = this.async();
-
       this.fs.copy(
           this.templatePath('Gruntfile.js'),
           this.destinationPath('Gruntfile.js')
       );
-      
-      var editor = new GruntfileEditor();
-      // TODO 
-      // this.options.parent.gruntDependencies
-      _.forEach(this.options.gruntDependencies, function(value, key1) {
-        _.forEach(value, function(value, gruntModule) {
-          console.log(gruntModule);
-          this.fs.copy(
-              this.templatePath('tasks/config/' + gruntModule + '.js'),
-              this.destinationPath('tasks/config/' + gruntModule + '.js')
-          );
-        });
+      var that = this;
+      _.forEach(gruntTasks, function(gruntModule) {
+        that.fs.copy(
+          gruntModule.pluginContext.templatePath('tasks/config/' + gruntModule.task + '.js'),
+          that.destinationPath('tasks/config/' + gruntModule.task + '.js')
+        );
       });
       done();
     }
