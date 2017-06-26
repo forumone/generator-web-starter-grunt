4c62a0bdf678172911fb0b1294073d7b38ff6c03
William Hurley <whurley@forumone.com>
2017-06-26 17:15:42 -0400

    Adds ability for other sub-generators to register watch tasks

--- web-starter/index.js
+++ web-starter/index.js
@@ -1,55 +1,123 @@
 'use strict';
-var generators = require('yeoman-generator'), 
-  _ = require('lodash'),
-  Promise = require('bluebird'),
-  rp = require('request-promise'),
-  semver = require('semver'),
-  glob = Promise.promisify(require('glob')),
-  pkg = require('../package.json'),
-  GruntfileEditor = require('gruntfile-editor');
 
+var generators = require('yeoman-generator');
+var _ = require('lodash');
+var pkg = require('../package.json');
+var GruntfileEditor = require('gruntfile-editor');
+
+/**
+ * @type {Object<string, GruntFileEditor>}
+ */
 var gruntTasks = {};
+
+/**
+ * @type {Object<string, Object>}
+ */
 var registeredTasks = {};
+
+/**
+ * @type {Object<string, GruntFileEditor>}
+ */
 var registeredTasksEditor = {};
 
+/**
+ * @type {Array.string}
+ */
+var watchTasks = [];
+
+/**
+ * Creates a GruntFileEditor for a given Grunt task
+ *
+ * @param {string} task
+ * @return {GruntFileEditor}
+ */
+function getGruntTask(task) {
+  if (!_.has(gruntTasks, task)) {
+    gruntTasks[task] = new GruntfileEditor();
+  }
+
+  return gruntTasks[task];
+}
+
+/**
+ * Registers a task to be run during the watch phase
+ *
+ * @param {string} task
+ * @return {Array.string}
+ */
+function registerWatchTask(task) {
+  if (watchTasks.indexOf(task) === -1) {
+    watchTasks.push(task);
+  }
+
+  return watchTasks;
+}
+
+/**
+ * Registers a task to be run by Grunt
+ *
+ * @param {string} name
+ * @param {string} task
+ * @param {number} priority
+ * @return {GruntFileEditor}
+ */
+function registerTask(name, task, priority) {
+  if (!_.has(registeredTasks, name)) {
+    registeredTasks[name] = [];
+    registeredTasksEditor[name] = new GruntfileEditor();
+  }
+
+  // Allow multiple tasks to be added at once
+  if (_.isArray(task)) {
+    registeredTasks[name] = registeredTasks[name].concat(task);
+  } else {
+    registeredTasks[name].push({ task: task, priority: priority });
+  }
+  return registeredTasksEditor[name];
+}
+
 module.exports = generators.Base.extend({
-  initializing : function() {
+  initializing: function () {
     this.options.addDevDependency(pkg.name, '~' + pkg.version);
-    var that = this;
-    this.options.addPlugin("grunt", {
-      getGruntTask : function(task) {
-        if (!_.has(gruntTasks, task)) {
-          gruntTasks[task] = new GruntfileEditor();
-        }
 
-        return gruntTasks[task];
-      },
-      registerTask : function(name, task, priority) {
-        if (!_.has(registeredTasks, name)) {
-          registeredTasks[name] = [];
-          registeredTasksEditor[name] = new GruntfileEditor();
-        }
-
-        // Allow multiple tasks to be added at once
-        if (_.isArray(task)) {
-          registeredTasks[name] = registeredTasks[name].concat(task);
-        }
-        else {
-          registeredTasks[name].push({ task : task, priority : priority });
-        }
-        return registeredTasksEditor[name];
-      }
+    this.options.addPlugin('grunt', {
+      getGruntTask: getGruntTask,
+      registerTask: registerTask,
+      registerWatchTask: registerWatchTask,
     });
   },
-  configuring : {
-    addDefautTasks : function() {
+  configuring: {
+    addDefautTasks: function () {
       this.options.addDevDependency('grunt', '^0.4.5');
       this.options.addDevDependency('include-all', '^0.1.6');
       this.options.addDevDependency('load-grunt-tasks', '^3.2.0');
+      this.options.addDevDependency('grunt-simple-watch', '^0.1.3');
+      this.options.addDevDependency('grunt-contrib-watch', '^1.0.0');
+      this.options.addDevDependency('grunt-concurrent', '^2.3.1');
+
+      var concurrent = getGruntTask('concurrent');
+      concurrent.insertConfig('concurrent.watch', JSON.stringify({
+        tasks: watchTasks,
+        options: {
+          logConcurrentOutput: true,
+        },
+      }));
+
+      concurrent.loadNpmTasks('grunt-concurrent');
+
+      registerTask('default', [{
+        task: 'build',
+        priority: 50,
+      }]);
+
+      registerTask('default', [{
+        task: 'concurrent:watch',
+        priority: 99,
+      }]);
     },
   },
-  writing : {
-    writeGruntFile : function() {
+  writing: {
+    writeGruntFile: function () {
       var done = this.async();
 
       this.fs.copyTpl(
@@ -59,25 +127,25 @@ module.exports = generators.Base.extend({
       );
       done();
     },
-    taskConfig : function() {
+    taskConfig: function () {
       var done = this.async();
       var that = this;
 
-      _.each(gruntTasks, function(editor, task) {
+      _.each(gruntTasks, function (editor, task) {
         that.fs.write(that.destinationPath('tasks/config/' + task + '.js'), editor.toString());
       });
 
       done();
     },
-    registeredTasks : function() {
+    registeredTasks: function () {
       var done = this.async();
       var that = this;
 
-      _.each(registeredTasks, function(tasks, name) {
+      _.each(registeredTasks, function (tasks, name) {
         var editor = registeredTasksEditor[name];
         var sorted = _.chain(tasks)
           .sortBy(['priority'])
-          .map(function(task) {
+          .map(function (task) {
             return task.task;
           })
           .value();
@@ -87,6 +155,6 @@ module.exports = generators.Base.extend({
       });
 
       done();
-    }
-  }
+    },
+  },
 });
